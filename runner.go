ddb5eb25a730d39bd7a1304d2f1b4355ee459c34
ondi <thy321@gmail.com>
2023-11-18 23:34:20 +0300

    minor fix

--- runner.go
+++ runner.go
@@ -145,17 +145,17 @@ func (self *Runner_t) RunAnyFun(count int, ts time.Time, entry Entry_t, do Do, d
 	return
 }
 
-// func (self *Runner_t) Remove(ts time.Time, service string, pack Pack) (removed int) {
-// 	self.mx.Lock()
-// 	pack_len := pack.Len()
-// 	for i := 0; i < pack_len; i++ {
-// 		if _, ok := self.cx.Remove(ts, Filter_t{Service: service, Id: pack.IDString(i)}); ok {
-// 			removed++
-// 		}
-// 	}
-// 	self.mx.Unlock()
-// 	return
-// }
+func (self *Runner_t) Remove(ts time.Time, service string, pack Pack) (removed int) {
+	self.mx.Lock()
+	pack_len := pack.Len()
+	for i := 0; i < pack_len; i++ {
+		if _, ok := self.cx.Remove(ts, Filter_t{Service: service, Id: pack.IDString(i)}); ok {
+			removed++
+		}
+	}
+	self.mx.Unlock()
+	return
+}
 
 func (self *Runner_t) run() {
 	defer self.wg.Done()
